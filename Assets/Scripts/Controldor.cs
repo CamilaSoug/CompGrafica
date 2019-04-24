d243430e8f3aff6a92f0529851a587808f711381
Camila Souza <camilagoncalves@alunos.utfpr.edu.br>
2019-04-24 19:57:44 -0300

    Commit inicial

---
+++ Assets/Scripts/Controldor.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controldor : MonoBehaviour
+{
+
+    public Transform jogador;
+    public Transform jogadorCam;
+    public float forca = 400f;
+    public float reescala = 0.025f;
+    bool temJogador = false;
+    bool beingCarried = false;
+    private bool tocado = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(gameObject.transform.position, jogador.position);
+        if (dist <= 2.5f)
+        {
+            temJogador = true;
+        }
+        else
+        {
+            temJogador = false;
+        }
+
+        if (temJogador && Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.parent = jogadorCam;
+            beingCarried = true;
+
+        }
+        if (beingCarried && Input.GetMouseButtonUp(0))
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            transform.parent = null;
+            beingCarried = false;
+        }
+
+
+        if (beingCarried)
+        {
+            if (tocado)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                tocado = false;
+            }
+            
+            if (Input.GetMouseButtonDown(1))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                GetComponent<Rigidbody>().AddForce(jogadorCam.forward * forca);
+            }
+            else if (Input.GetMouseButtonDown(2))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+            {
+                transform.localScale += new Vector3(reescala, reescala, reescala);
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+            {
+                transform.localScale -= new Vector3(reescala, reescala, reescala);
+            }
+        }
+    }
+    void OnTriggerEnter()
+    {
+        if (beingCarried)
+        {
+            tocado = true;
+        }
+    }
+}
---
+++ Assets/Scripts/FPS.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+
+    private Rigidbody rbd;
+    public float velocidade = 10f;
+    public float velocidadeRot = 50f;
+    public float pulo = 100f;
+    private float rotY = 0;
+    private float rotX = 0;
+    public Text mira;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbd = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mira.text = "(+)";
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        rotY += Input.GetAxis("Mouse X") * velocidadeRot * Time.deltaTime;
+        rotX -= Input.GetAxis("Mouse Y") * velocidadeRot * Time.deltaTime;
+        rbd.velocity = transform.TransformDirection(new Vector3(x * velocidade, rbd.velocity.y, z * velocidade));
+        if (Input.GetKeyDown(KeyCode.Space))
+            rbd.AddForce(0, pulo, 0);
+        transform.localEulerAngles = new Vector3(0, rotY, 0);
+        Camera.main.transform.localEulerAngles = new Vector3(Mathf.Clamp(rotX, -70, 70), 0, 0);
+    }
+}
